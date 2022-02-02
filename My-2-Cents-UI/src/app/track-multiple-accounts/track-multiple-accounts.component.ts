a0933e5e35e68e33a36bcbe32f15af5c975cedb2
Jingjing Zhuang <jzhuang3219@gmail.com>
2022-02-02 16:31:32 -0500

    Test for auth0

--- My-2-Cents-UI/src/app/track-multiple-accounts/track-multiple-accounts.component.ts
+++ My-2-Cents-UI/src/app/track-multiple-accounts/track-multiple-accounts.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Injectable, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-track-multiple-accounts',
   templateUrl: './track-multiple-accounts.component.html',
-  styleUrls: ['./track-multiple-accounts.component.css']
+  styleUrls: ['./track-multiple-accounts.component.css'],
 })
 export class TrackMultipleAccountsComponent implements OnInit {
+  constructor(
+    public auth: AuthService,
+    @Inject(DOCUMENT) private doc: Document
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  logout(): void {
+    console.log(this.doc.location);
+    this.auth.logout({ returnTo: this.doc.location.origin });
+    alert('Successfully logout!');
   }
-
 }
