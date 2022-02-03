13a384c23c442d8cab2ea8e0b77cdf68bd81ed77
srod-85 <94401097+srod-85@users.noreply.github.com>
2022-02-03 10:58:24 -0800

    updating component code with additions to .ts, .css, and .html for user-change-info,. Added a button to change-info.html. Updates to app.module.ts

---
+++ My-2-Cents-UI/src/app/user-change-info/user-change-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserChangeInfoComponent } from './user-change-info.component';
+
+describe('UserChangeInfoComponent', () => {
+  let component: UserChangeInfoComponent;
+  let fixture: ComponentFixture<UserChangeInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserChangeInfoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserChangeInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
