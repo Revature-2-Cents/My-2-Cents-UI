11dd977ef9bd048444578b2b2b27d443702acf44
megan <postlemv@clarkson.edu>
2022-02-09 13:38:26 -0500

    merge errors fixed

--- My-2-Cents-UI/src/app/transfer-money/transfer-money.component.ts
+++ My-2-Cents-UI/src/app/transfer-money/transfer-money.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Account } from '../account';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { TransferService } from '../transfer-service';
-=======
-// import { TransferService } from '../transfer-service';
->>>>>>> c60b5fb1badbe7544a399945d8ebada9f19f49d3
-=======
 import { TransferService } from '../transfer.service';
->>>>>>> 4a8dbcf88d25aa13997454fac695b79fd2e718d3
 
 @Component({
   selector: 'app-transfer-money',
@@ -19,31 +11,15 @@ export class TransferMoneyComponent implements OnInit {
   account: Account[] = [];
   funds: boolean = true;
 
-<<<<<<< HEAD
-  accounts: Account[] = [];
-  funds: boolean = true;
 
   @Output() fromAccount = new EventEmitter<string>();
   @Output() toAccount = new EventEmitter<string>();
   @Output() quantity = new EventEmitter<number>();
 
-<<<<<<< HEAD
-  constructor() { }
-=======
-  @Output() fromAccount = new EventEmitter<string>();
-  @Output() toAccount = new EventEmitter<string>();
-  @Output() quantity = new EventEmitter<number>();
->>>>>>> c60b5fb1badbe7544a399945d8ebada9f19f49d3
-=======
-  constructor(private transferService: TransferService) { }
->>>>>>> 4a8dbcf88d25aa13997454fac695b79fd2e718d3
-
-  constructor() {}
-
-<<<<<<< HEAD
+  constructor(private transferService: TransferService) {}
   CheckFunds(fromAccount: number, toAccount: number, quantity: number) {
     
-    if (+this.accounts[+fromAccount].TotalBalance < quantity) {
+    if (+this.account[+fromAccount].TotalBalance < quantity) {
       this.funds = false;
     } else {
       this.TransferFunds(+fromAccount, +toAccount, quantity);
@@ -55,8 +31,5 @@ export class TransferMoneyComponent implements OnInit {
 
   }
 
-}
-=======
   ngOnInit(): void {}
 }
->>>>>>> c60b5fb1badbe7544a399945d8ebada9f19f49d3
