98bc02b4595d75f69b7ff04c022841ff7a572512
Edward cicio <edwardcicioc@gmail.com>
2022-02-11 10:48:13 -0500

    Add httpclient in onInit()

--- My-2-Cents-UI/src/app/track-income/track-income.component.ts
+++ My-2-Cents-UI/src/app/track-income/track-income.component.ts
@@ -10,14 +10,30 @@ import { IncomesService } from '../incomes.service';
 })
 export class TrackIncomeComponent implements OnInit {
 
-  Items : Expenses[] = Mock_Items;
+  //Items : Expenses[] = Mock_Items;
 
   selectedItem?: Expenses;
 
-  testHttp = this.iService.getAccountInfo();
+  Items: Expenses[] = [];
   constructor(private iService : IncomesService) { }
 
   ngOnInit(){
+    // will automatically get information
+    this.iService.getAccountInfo('5').then((datas) => {
+      let testIncome: Expenses;
+
+      // For loop to assign testIncome with necessary data
+      // Use testIncome to add the information
+      for(let i = 0; i < datas.length; i++)
+      {
+        testIncome = { Name: datas[i].accountType, Amount: datas[i].totalBalance, ItemName: "test", 
+        Price: datas[i].amount, Detail: datas[i].transactionName, showDate: datas[i].transactionDate }
+        this.iService.addToIncome(testIncome);
+      }
+    })
+
+    this.Items = this.iService.getIncomes();
+    console.log(this.Items);
   }
 
   selectItem(Item: Expenses): void {
