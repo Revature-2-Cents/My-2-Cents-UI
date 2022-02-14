f941c4a4474ca73fe05e58c90956db9f0fbae5cb
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-02-14 15:03:37 -0600

    merging

--- My-2-Cents-UI/src/app/transfer.service.ts
+++ My-2-Cents-UI/src/app/transfer.service.ts
@@ -4,20 +4,6 @@ import { Observable } from 'rxjs';
 import { Account } from './account';
 
 @Injectable({
-<<<<<<< HEAD
-  providedIn: 'root'
-})
-export class TransferService {
-  private Url = "https://my2centsapi.azurewebsites.net/api/Transaction";
-
-  TransferFunds(fromAccount: number, toAccount: number, quantity: number): void {
-    this.http.post<any>(this.Url, {to: 'fromAccount', from: 'toAccount', amount: 'quantity'}).subscribe(response => { console.log(response.status)});
-  }
-
-  constructor(private http: HttpClient) { }
-
-
-=======
   providedIn: 'root',
 })
 export class TransferService {
@@ -32,5 +18,4 @@ export class TransferService {
   }
 
   constructor(private http: HttpClient) {}
->>>>>>> 407caa5473fde717e4f04a8a9cadf41fcc73c17a
 }
