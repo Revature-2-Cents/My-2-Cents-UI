d7430e10ad17820a018003bc2e7501b0d55d52c5
Edward cicio <edwardcicioc@gmail.com>
2022-02-14 17:41:01 -0500

    Button back and Transaction history working

--- My-2-Cents-UI/src/app/mock-incomes.ts
+++ My-2-Cents-UI/src/app/mock-incomes.ts
@@ -10,30 +10,3 @@ export interface Incomes {
     transactionId: number;
     transactionName: string;
 }
-
-export interface Expenses {
-    Name: string;
-    Amount: number;
-    ItemName: string;
-    Price: number;
-    Detail: string;
-    showDate: string;
-}
-
-export const Mock_Items: Expenses[] = [
-    {
-    'Name': 'Account',
-    'Amount': 100.00,
-    'ItemName': 'TestItem',
-    'Price': 10.00,
-    'Detail': 'Purchased today',
-    'showDate': '20220202'
-    },{
-    'Name': 'Account',
-    'Amount': 100.00,
-    'ItemName': 'TestItem2',
-    'Price': -20.00,
-    'Detail': 'Purchased Yesterday',
-    'showDate': '20220202'
-    }
-];
