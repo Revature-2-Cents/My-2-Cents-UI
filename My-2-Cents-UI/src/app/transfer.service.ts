3f345ac608d833880cee7fbf97226d8f624a8f51
Jingjing Zhuang <jzhuang3219@gmail.com>
2022-02-14 13:00:18 -0500

    able to display on dropdown menu

--- My-2-Cents-UI/src/app/transfer.service.ts
+++ My-2-Cents-UI/src/app/transfer.service.ts
@@ -4,16 +4,22 @@ import { Observable } from 'rxjs';
 import { Account } from './account';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TransferService {
-  private Url = "https://my2centsapi.azurewebsites.net/api/Transaction";
+  private Url = 'https://my2centsapi.azurewebsites.net/api/Transaction';
 
-  TransferFunds(fromAccount: number, toAccount: number, quantity: number): void {
-    this.http.post<any>(this.Url, {to: 'fromAccount', from: 'toAccount', amount: 'quantity'}).subscribe(response => { console.log(response.status)});
+  TransferFunds(
+    fromAccount: number,
+    toAccount: number,
+    quantity: number
+  ): void {
+    this.http.post<any>(this.Url, {
+      to: fromAccount,
+      from: toAccount,
+      amount: quantity,
+    });
   }
 
-  constructor(private http: HttpClient) { }
-
- 
+  constructor(private http: HttpClient) {}
 }
