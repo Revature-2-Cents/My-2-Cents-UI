f07fb0dab07e328c964024ee644473061f709fc7
srod-85 <94401097+srod-85@users.noreply.github.com>
2022-02-13 19:07:29 -0800

    Added route to create-profile

--- My-2-Cents-UI/src/app/app-routing.module.ts
+++ My-2-Cents-UI/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserChangeInfoComponent } from './user-change-info/user-change-info.component';
 import { TrackExpensesComponent } from './track-expenses/track-expenses.component';
 import { TrackIncomeComponent } from './track-income/track-income.component';
+import { CreateProfileComponent } from './create-profile/create-profile.component';
 
 const routes: Routes = [
   {
@@ -33,6 +34,11 @@ const routes: Routes = [
   {
     path: 'user-change-info',
     component: UserChangeInfoComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'create-profile',
+    component: CreateProfileComponent,
     canActivate: [AuthGuard],
   },
   {
