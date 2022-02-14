d5360f018a8becead1a48d484e8dba5c122737f4
srod-85 <94401097+srod-85@users.noreply.github.com>
2022-02-14 08:15:17 -0800

    Adopting code from team Earth for transfer-money. Still getting some errors. Needing transfer-money service file

--- My-2-Cents-UI/src/app/transfer-money/transfer-money.component.ts
+++ My-2-Cents-UI/src/app/transfer-money/transfer-money.component.ts
@@ -1,6 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Account } from '../account';
-// import { TransferService } from '../transfer-service';
+import { TransferService } from '../transfer.service';
+import { Location } from '@angular/common';
+
+import { TestBed, async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-transfer-money',
@@ -16,9 +20,50 @@ export class TransferMoneyComponent implements OnInit {
   @Output() toAccount = new EventEmitter<string>();
   @Output() quantity = new EventEmitter<number>();
 
-  constructor() {}
+  constructor(private transferService: TransferService, private http: HttpClient, private location: Location) {}
 
   ngOnInit(): void {
     console.log(this.account);
+  }
+
+  CheckFunds(fromAccount: number, toAccount: number, quantity: number): boolean {
+    if (fromAccount == toAccount) {
+      alert("Cannot Transfer From and To Same Account");
+      return false;
+    } else {
+      let fromAcc;
+
+      for (let i = 0; i < this.account.length; i++) {
+        if (this.account[i].AccountID == fromAccount) {
+          fromAcc = this.account[i];
+          break;
+        }
+      }
+
+      if (fromAcc != undefined) {
+        if (+fromAcc.TotalBalance < quantity) {
+          this.funds = false;
+          return false;
+        } else {
+          this.TransferFunds(+fromAccount, +toAccount, quantity).subscribe((response: { status: any; body: number; }) => {
+            this.http.jsonp;
+            console.log(response.status)
+            if (response.body > 0) {
+              this.location.back();
+              return true;
+            } else {
+              alert("Server Error Please Contact the Bank for Assistance");
+              return false;
+            }
+          });
+        }
+      }
+    }
+
+    return false;
+  }
+
+  TransferFunds(fromAccount: number, toAccount: number, quantity: number) : any {
+    return this.transferService.TransferFunds(fromAccount, toAccount, quantity);
   }
 }
