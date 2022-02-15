b8c11e3074cdb1a6dcccd3db44bd9eb42abe2e68
shaul stavi <shaulstavi@gmail.com>
2022-02-15 17:26:51 -0500

    test income comp

--- My-2-Cents-UI/src/app/track-income/track-income.component.spec.ts
+++ My-2-Cents-UI/src/app/track-income/track-income.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { TrackIncomeComponent } from './track-income.component';
+import { ActivatedRoute } from '@angular/router';
+import { IncomesService } from '../incomes.service';
+import * as Rx from 'rxjs';
+import { delay } from "rxjs/operators";
+import { Incomes } from '../mock-incomes'
 
 describe('TrackIncomeComponent', () => {
   let component: TrackIncomeComponent;
@@ -8,18 +13,32 @@ describe('TrackIncomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TrackIncomeComponent ]
+      declarations: [ TrackIncomeComponent ],
+      providers: [{ provide: IncomesService, useValue: { getAccountInfo() { } } },
+        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: {get:(AccountID:number)=>{AccountID:1}}}  }}
+      ], 
     })
-    .compileComponents();
+    .compileComponents();    
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TrackIncomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(TrackIncomeComponent);
+    const component = fixture.debugElement.componentInstance;
     expect(component).toBeTruthy();
   });
+
+
+  it('should call ngOnInit', () => {
+    const fixture = TestBed.createComponent(TrackIncomeComponent);
+    const component = fixture.debugElement.componentInstance;    
+    const service = fixture.debugElement.injector.get(IncomesService);    
+    let a:Incomes[]=[];
+    let spy_getAccountInfo = spyOn(service,"getAccountInfo").and.returnValue(Promise.resolve(a));
+    component.ngOnInit();
+    expect(component.Items).toEqual([]);
+  })
+
+
+
 });
