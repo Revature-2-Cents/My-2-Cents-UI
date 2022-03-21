0b7500bb6d0b116765c9fa10b045c7f7af691e51
Sid Hinson <octobersith@outlook.com>
2022-03-21 18:13:37 -0400

    Initial HTML etc

--- My-2-Cents-UI/src/app/budget-calculator/budget-calculator.component.ts
+++ My-2-Cents-UI/src/app/budget-calculator/budget-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-budget-calculator',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-calculator.component.css']
 })
 export class BudgetCalculatorComponent implements OnInit {
+  budgetFormGroup = new FormGroup({
+    income: new FormControl(0),
+    expenses: new FormControl(0),
+    wants: new FormControl(0),
+    savings: new FormControl(0)
+  })
+
+  userIncome = 0;
+  userExpenses = 0;
+  userWants = 0;
+  userSavings = 0;
+
 
   constructor() { }
 
   ngOnInit(): void {
+
+
+  }
+
+  onClick () {
+    
   }
 
 }
+
+
