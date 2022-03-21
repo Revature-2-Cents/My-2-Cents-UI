54240da8f70d3b75bdc04866d18ac17e1f27ded0
hsprime85 <hsprime85@gmail.com>
2022-03-21 13:30:58 -0700

    adding order history in investemnt portfolio

--- My-2-Cents-UI/src/app/app.module.ts
+++ My-2-Cents-UI/src/app/app.module.ts
@@ -17,7 +17,6 @@ import { UserChangeInfoComponent } from './user-change-info/user-change-info.com
 
 import { CreateProfileComponent } from './create-profile/create-profile.component';
 import { FormsModule } from '@angular/forms';
-import { InvestmentPortfolioComponent } from './investment-portfolio/investment-portfolio.component';
 import { InvestmentPortfolioMainComponent } from './investment-portfolio/investment-portfolio-main/investment-portfolio-main.component';
 import { InvestmentPortfolioTableComponent } from './investment-portfolio/investment-portfolio-table/investment-portfolio-table.component';
 import { OrderHistoryComponent } from './investment-portfolio/order-history/order-history.component';
@@ -33,7 +32,6 @@ import { OrderHistoryComponent } from './investment-portfolio/order-history/orde
     TrackMultipleAccountsComponent,
     UserChangeInfoComponent,
     CreateProfileComponent,
-    InvestmentPortfolioComponent,
     InvestmentPortfolioMainComponent,
     InvestmentPortfolioTableComponent,
     OrderHistoryComponent
