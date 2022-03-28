18325a5ec91bdabf98297d5631a47c9a13a3789a
Kristopher Wasserman <kristopherwasserman6@msn.com>
2022-03-28 15:11:16 -0500

    added calulations to budget

--- My-2-Cents-UI/src/app/budget-info/budget-info.component.ts
+++ My-2-Cents-UI/src/app/budget-info/budget-info.component.ts
@@ -13,23 +13,37 @@ export class BudgetInfoComponent implements OnInit {
   userExpenses: number = 0;
 
   displayBudgetInfo = true; //this property will be used to display the correct user budget information like income,expenses,wants,and savings in conjunction with *ngIf 
-  income: number = this.userIncome;
-
-  expenses: number = this.userExpenses;
-
-  wants: number = (this.userIncome - this.userExpenses) * 0.6;
-
-  savings: number = (this.userIncome - this.userExpenses) * 0.4;
+  income: number;
+  expenses: number;
+  wants: number;
+  savings: number;
 
   //change these to be percentages of Income
   expensesPercentage: number = 0;
-  wantsPercentage: number = 0;
-  savingsPercentage: number = 0;
+  wantsPercentage:number = 0;
+  savingsPercentage:number = 0;
 
 
+  createPercentage(){
+
+    this.income = this.userIncome;
+    this.wants = (this.userIncome - this.userExpenses) * 0.6;
+    this.savings = (this.userIncome - this.userExpenses) * 0.4;
+
+    this.expenses = this.userExpenses;
+    this.expensesPercentage = this.expenses / this.income * 100;
+    this.wantsPercentage =  this.wants / this.income * 100;
+    this.savingsPercentage = this.savings / this.income * 100;
+  
+  }
+  
+  
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.createPercentage();
   }
 
+ 
 }
