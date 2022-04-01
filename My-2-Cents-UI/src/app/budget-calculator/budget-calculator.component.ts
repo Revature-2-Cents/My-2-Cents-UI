bcc0af19e57e9f2dfd62ffe31c66ce99fd2ecea3
TerranceUsher <crushableskull@gmail.com>
2022-04-01 12:27:06 -0500

    Terrance changes 4/1/22

--- My-2-Cents-UI/src/app/budget-calculator/budget-calculator.component.ts
+++ My-2-Cents-UI/src/app/budget-calculator/budget-calculator.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit, NgModule } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { delay } from 'rxjs';
+import { BudgetChartComponent } from '../budget-chart/budget-chart.component';
+import { BudgetInfoComponent } from '../budget-info/budget-info.component';
 // import { Chart } from 'node_modules/chart.js';
 @Component({
   selector: 'app-budget-calculator',
@@ -20,6 +23,7 @@ export class BudgetCalculatorComponent implements OnInit {
   userWants: number = 0;
   userSavings: number = 0;
   useChart: boolean = false;
+  buttonCheck: boolean = false;
 
   constructor() {
   }
@@ -32,7 +36,14 @@ export class BudgetCalculatorComponent implements OnInit {
     console.log(this.userIncome);
     this.userExpenses = budgetFormGroup.get("expenses").value;
     console.log(this.userExpenses);
-    this.useChart = !this.useChart;
+    // if (!this.useChart)
+    // {
+      this.useChart = !this.useChart;
+    // }
+    // else
+    // {
+    //   this.buttonCheck = !this.buttonCheck;
+    // }
   }
 
 
